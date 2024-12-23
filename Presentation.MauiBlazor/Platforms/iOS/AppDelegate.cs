d956dabc173d3b502e8e5031aee7a3f6aa2da8b8
Oscar Hederlig <oscar.hederlig@gmail.com>
2024-12-23 08:42:53 +0100

    Add project files.

---
+++ Presentation.MauiBlazor/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Presentation.MauiBlazor
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
